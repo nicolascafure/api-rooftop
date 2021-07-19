92e3a402aac9de1cc2e828fd60379f12b3fa947b
Nico Cafure <nicocafure@gmail.com>
2021-07-19 16:49:56 -0300

    comenzando api de imagenes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 
 
 function App() {
 
+const [image, setImage]=useState({})
+
+
 const consultarApi=()=>{
 const data = fetch("https://fakerapi.it/api/v1/images?_quantity=1&_type=kittens&_height=300")
 .then(data=>data.json())
-.then(resultado=>console.log(resultado))
-
-
+.then(resultado=>setImage(resultado.data[0]))
 
 }
+
+
+
+
 
   return (
     <div className="contenedor-principal">
