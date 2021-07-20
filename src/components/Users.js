184a66ce22db4fbb253b98160272435c0b0a3f0a
Nicolas Cafure <nicocafure@gmail.com>
2021-07-19 23:30:29 -0300

    dandole forma al render de los users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,15 +1,27 @@
-const Users = () => {
+import { useState } from "react"
 
+const Users = () => {
+const [users, setUsers] = useState([])
     const getUsers = ()=>{
-fetch("https://fakerapi.it/api/v1/users?_quantity=1&_gender=male")
+fetch("https://fakerapi.it/api/v1/users?_quantity=10&_gender=male")
 .then(data=>data.json())
-.then(data=>console.log(data))
+.then(data=>setUsers(data.data))
 
     }
 
     return ( 
 <div className="contenedor-users">
 <button onClick={getUsers}>getUsers</button>
+
+{users.map(user=><div key={user.uuid} className="user">
+<p className="user-data">{user.username}</p>
+<p className="user-data">{user.firstname}</p>
+<p className="user-data">{user.lastname}</p>
+<p className="user-data">{user.email}</p>
+<p className="user-data">{user.website}</p>
+
+
+</div>)}
 
     
 </div>
